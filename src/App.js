4e0aa8aab1ff805a0c5c4c3934bdf505ec037a62
mateNikula <31796337+matenikula@users.noreply.github.com>
2020-04-16 08:27:55 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Example</h1>
+      <h2>This is from codesandbox!</h2>
+    </div>
+  );
+}
